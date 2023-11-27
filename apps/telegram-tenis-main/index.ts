3f2b2075eadd244c18782242f01c2904c1f3db80
Илья <marchenko.ilya99@gmail.com>
2023-11-27 19:50:34 +0200

    refactor index.ts

--- apps/telegram-tenis-main/index.ts
+++ apps/telegram-tenis-main/index.ts
@@ -10,50 +10,22 @@ const token = process.env.TOKEN || 'Token-NOT-FOUND'
 const bot = new TelegramBot(token, {polling:true})
 
 bot.on('message', msg =>{
-  const UserData : UserData = initUserData(msg);
-
-  const CommandData : CommandData | [] = parserCommand(UserData.messageText)
-  if(!Array.isArray(CommandData)){
-    const {command} = CommandData
-    const hendler = commandHandlers[command as Command]
-    hendler?hendler(bot,UserData,CommandData):commandHandlers['def'](bot,UserData,CommandData)
-  }
-  else{
-    bot.sendMessage(UserData.userId, 'Ответочка')
-  }
+  const textMessage = msg.text
+  hendlerCommand(textMessage, msg);
 })
+
 bot.on('callback_query', (query) => {
-  const msg = query.message;
-  const UserData : UserData = initUserData(msg);
+  const msg = query.message;  
   const chatId = query?.message?.chat.id;
   const messageId = query.message?.message_id;
-  if(chatId && messageId)
-  // Удаление сообщения после нажатия на кнопку
-  bot.deleteMessage(chatId, messageId)
-    .then(() => {
-      console.log('Сообщение успешно удалено.');
-    })
-    .catch((error) => {
-      console.error('Ошибка при удалении сообщения:', error);
-    });
   
+  removeMessage(chatId,messageId)
   const buttonData = query.data;
-  console.log(`Debug  msg ${msg}  |  buttonDATA ${buttonData}`)
-  if(buttonData){
-    const CommandData : CommandData | [] = parserCommand(buttonData)
-    console.log("Command", CommandData);
-    if(!Array.isArray(CommandData)){
-      const {command} = CommandData
-      const hendler = commandHandlers[command as Command]
-      hendler?hendler(bot,UserData,CommandData):commandHandlers['def'](bot,UserData,CommandData)
-    }
-    else{
-      bot.sendMessage(UserData.userId, 'Ответочка после кнопки')
-    }
-  }
+  hendlerCommand(buttonData,msg)
+  
 });
 
-const parserCommand = (msg:string) : CommandData | [] => {
+const parserCommand = (msg:string) : CommandData | null => {
   if (msg?.startsWith('/')) {
     const match = msg.match(/^\/(\w+)(?:\s+([\s\S]*))?$/);
     if(match)
@@ -62,10 +34,10 @@ const parserCommand = (msg:string) : CommandData | [] => {
       params:match?.[2]?.split(/\s+/) 
     }
     else{
-      return []
+      return null
     }
   }else{
-    return []
+    return null
   }
 }
 const initUserData = (msg:TelegramBot.Message|undefined)=>{
@@ -74,5 +46,29 @@ const initUserData = (msg:TelegramBot.Message|undefined)=>{
     messageText: msg?.text ||'',
     userFirstName: msg?.chat.first_name||"userName NotFound"
   }
+}
+const hendlerCommand = (text:string|undefined , message: TelegramBot.Message|undefined) => {
+  if(text && message){
+    const UserData : UserData = initUserData(message);
+    const CommandData : CommandData | null = parserCommand(text)
+    if(CommandData){
+      const {command} = CommandData
+      const hendler = commandHandlers[command as Command]
+      hendler?hendler(bot,UserData,CommandData):commandHandlers['def'](bot,UserData,CommandData)
+    }
+    else{
+      bot.sendMessage(UserData.userId, 'Ответочка')
+    }
+  }
+}
+const removeMessage =(chatId:number|undefined,messageId:number|undefined)=>{
+  if(chatId && messageId)
+  bot.deleteMessage(chatId, messageId)
+    .then(() => {
+      console.log('Сообщение успешно удалено.');
+    })
+    .catch((error) => {
+      console.error('Ошибка при удалении сообщения:', error);
+    }); 
 }
 
