9f99ebef4e7676258762971f4273a1048a319b90
Илья <marchenko.ilya99@gmail.com>
2024-01-29 23:23:46 +0200

    add entity player

---
+++ apps/telegram-tenis-main/entities/Player.ts
@@ -0,0 +1,17 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column
+} from 'typeorm';
+
+@Entity()
+export class Player {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  username?: string;
+
+  @Column()
+  TelegramId!: number;  
+}
