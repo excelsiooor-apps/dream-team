0cf1fad138b5d82cf1995f0b214242ce9206f9ab
Илья <marchenko.ilya99@gmail.com>
2024-02-11 20:12:32 +0200

    correct-connectFile

--- apps/telegram-tenis-main/connector.ts
+++ apps/telegram-tenis-main/connector.ts
@@ -1,23 +1,33 @@
 import { createConnection, Connection, ConnectionOptions } from 'typeorm';
-import { User } from './entities/User';
-import { Game } from './entities/Game';
-import { Status } from './entities/Status';
+
+import { Match } from './entities/Match';
+import { MatchResult } from './entities/MatchResult';
+import { Mode } from './entities/Mode';
+import { Player } from './entities/Player';
+import { Rating } from './entities/Rating';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+
 
 export async function connectDatabase(): Promise<Connection> {
   const connectionOptions: ConnectionOptions = {
-    type: 'sqlite',
-    database: './db/database.sqlite',
-    synchronize: true,
-    logging: true,
-    entities: [User,Game,Status],
+    type: 'postgres',
+    host: process.env.DB_HOST, 
+    port: Number(process.env.DB_PORT) || 1234, 
+    username: process.env.DB_USER, 
+    password:  process.env.DB_PASS, 
+    database:  process.env.DB_DB_NAME, 
+    synchronize: true, // Устанавливаем synchronize в true для автоматической синхронизации схемы базы данных (только для разработки)
+    logging: true, // Включаем логирование SQL запросов (можно отключить в продакшене)
+    entities: [Match, MatchResult, Mode, Player, Rating],
   };
 
-  const connection = await createConnection(connectionOptions);
-  console.log('Connected to the database');
-  return connection;
-}
-
-export async function disconnectDatabase(connection: Connection): Promise<void> {
-  await connection.close();
-  console.log('Disconnected from the database');
+  try {
+    const connection = await createConnection(connectionOptions);
+    console.log('Connected to PostgreSQL database');
+    return connection;
+  } catch (error) {
+    console.error('Error connecting to PostgreSQL database:', error);
+    throw error;
+  }
 }
