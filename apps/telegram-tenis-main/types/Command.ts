cad84d76824ac3ae24519fdfcf0723fa380458be
Илья <marchenko.ilya99@gmail.com>
2024-02-17 21:16:52 +0200

    added command rating in command list

--- apps/telegram-tenis-main/types/Command.ts
+++ apps/telegram-tenis-main/types/Command.ts
@@ -1,9 +1,11 @@
+import { UserDatails } from './UserDetails';
 import TelegramBot from "node-telegram-bot-api";
 import { StartCommand } from "../commands/StartCommand";
+import { RatingCommand } from '../commands/RatingCommand';
 
 
 export interface Command {
-  execute(bot: TelegramBot, commandDetails: CommandDetails): void;
+  execute(bot: TelegramBot, commandDetails: CommandDetails, userDatails: UserDatails): void;
 }
 export interface CommandDetails {
   command: CommandList,
@@ -11,13 +13,13 @@ export interface CommandDetails {
 }
 export enum CommandList {
   Start = 'start',
+  Rating = 'rating',
   // Help = 'help',
   // Register = 'register',
   // OneVsOne = '1x1',
   // Add = 'add',
   // Yes = 'yes',
   // No = 'no',
-  // Rating = 'rating',
   // Match = 'match',
   // EndGame = 'endGame',
   // GG = "gg",
@@ -29,4 +31,5 @@ export enum CommandList {
 }
 export const CommandStorage: Record<CommandList, Command> = {
   [CommandList.Start]: new StartCommand(),
+  [CommandList.Rating]: new RatingCommand()
 };
