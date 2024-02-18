2b6b010ac1a729f15ad2802843fea69fd4511bd3
Илья <marchenko.ilya99@gmail.com>
2024-02-18 18:42:01 +0200

    correct rating command

--- apps/telegram-tenis-main/commands/RatingCommand.ts
+++ apps/telegram-tenis-main/commands/RatingCommand.ts
@@ -2,9 +2,14 @@ import TelegramBot from "node-telegram-bot-api"
 import { Command, CommandDetails } from "../types/Command"
 import { UserDatails } from "../types/UserDetails"
 import { MESSAGES } from "../utils/consts/Messages"
+import { PlayerService } from "../services/playerService"
+
 
 export class RatingCommand implements  Command {  
-  execute(bot: TelegramBot, commandDetails: CommandDetails, userDatails: UserDatails): void {
-    bot.sendMessage(userDatails.userId, MESSAGES.RatingMessage())
+  async execute(bot: TelegramBot, commandDetails: CommandDetails, userDatails: UserDatails): Promise<void> {
+    const playerService = new PlayerService();
+    const player = await playerService.GetByTelegramId(userDatails.telegramId);
+    const rating = player?.rating
+    bot.sendMessage(userDatails.telegramId, MESSAGES.RatingMessage(rating))
   }
 }
