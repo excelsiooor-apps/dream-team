29853b3cc97dd75396019ffd733d51cc49cc7511
Илья <marchenko.ilya99@gmail.com>
2024-02-18 18:42:13 +0200

    correct start command

--- apps/telegram-tenis-main/commands/StartCommand.ts
+++ apps/telegram-tenis-main/commands/StartCommand.ts
@@ -1,14 +1,24 @@
 import TelegramBot from "node-telegram-bot-api";
 import { Command, CommandDetails } from "../types/Command";
 import { UserDatails } from '../types/UserDetails';
-import { keyboardHelp } from "../keyboards/keyboard";
 import { MESSAGES } from "../utils/consts/Messages";
+import { PlayerService } from "../services/playerService";
+import { Player } from "../entities/Player";
+
 
 export class StartCommand implements  Command {  
-  execute(bot: TelegramBot, commandDetails: CommandDetails, userDatails: UserDatails): void {
-    bot.sendMessage(userDatails.userId, MESSAGES.HelloMessage(userDatails.userName))
-    bot.sendMessage(userDatails.userId, 'Действия:', {
-      reply_markup: keyboardHelp
-    })
+  async execute(bot: TelegramBot, commandDetails: CommandDetails, userDatails: UserDatails): Promise<void> {
+    const playerService = new PlayerService();
+    const player = await playerService.GetByTelegramId(userDatails.telegramId);
+    if(player){
+      bot.sendMessage(userDatails.telegramId, MESSAGES.StartMessage(player.username), MESSAGES.KEYBOARD.START)
+    }
+    else{
+      const player = new Player();
+      player.TelegramId = userDatails.telegramId;
+      player.username = userDatails.userName;      
+      const result = await playerService.playerRepository.Create(player)
+      bot.sendMessage(result.TelegramId, MESSAGES.StartFerstMessage(result.username), MESSAGES.KEYBOARD.START)
+    }    
   }
 }
